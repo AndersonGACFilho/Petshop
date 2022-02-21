5cbcdd209768563a010e4ecd7f42f77e45fcf399
Anderson Filho <andersonfilho09@gmail.com>
2022-02-21 14:35:51 -0300

    third part of classes implementation

--- src/main/java/br/com/petshop/model/person/Specialization.java
+++ src/main/java/br/com/petshop/model/person/Specialization.java
@@ -3,18 +3,19 @@ package br.com.petshop.model.person;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-
+@Entity
 public class Specialization {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String description;
-	@OneToMany(mappedBy = "veterinarian")
+	@OneToMany
 	private List<Veterinarian> veterinarians = new ArrayList<Veterinarian>();
 	
 	public String getName() {
